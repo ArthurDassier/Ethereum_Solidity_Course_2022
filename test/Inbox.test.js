a1ae82746ae1c654030272b6ddc0ab7e9a1b69cd
Arthur Dassier <dassierarthur@gmail.com>
2021-12-29 17:10:04 +0100

    [END OF SECTION 2]

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,6 +4,7 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
 
+
 let accounts = 0;
 
 beforeEach(async () => {
@@ -18,14 +19,23 @@ beforeEach(async () => {
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a defaut message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
     });
 });
 
 
-
 // ----------- SECTION 3 VIDEO 47 : TEST WITH MOCHA -----------
-
 
 // class Car {
 //     park() {
